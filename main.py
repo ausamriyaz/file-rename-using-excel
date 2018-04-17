4167815f53ff7562b5602b172ff2f6d320fd6023
Ausam Riyaz <dosfilthtab@gmail.com>
2018-04-17 20:32:04 +0530

    move to function

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from xlrd import open_workbook
 
-book = open_workbook(input('Enter file path with file name and extension: '))
-
-sheet = book.sheet_by_index(0)
-
 data = []
 
-for row in range(1, sheet.nrows):
-    temp = str(sheet.row_values(row, 0))
 
-    data.append(temp[2:-2])
+def appendTolist():
+    book = open_workbook(input('Enter file path with file name and extension: '))
+
+    sheet = book.sheet_by_index(0)
+
+    for row in range(1, sheet.nrows):
+        temp = str(sheet.row_values(row, 0))
+
+        data.append(temp[2:-2])
+
+
+appendTolist()
 
 print(data)
 
